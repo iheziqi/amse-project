8b063242bc421dad9b0f78206155dd6a7086fcc2
Ziqi He <heziqi4399@gmail.com>
2023-06-07 14:54:32 +0200

    Update data pipeline and change the name of sqlite database file

--- data/get_data.py
+++ data/get_data.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import urllib.request
 
+
 def get_data(url):
-	# Simulate a web browser request.
-	req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-	# Read the data into data frame
-	df = pd.read_csv(urllib.request.urlopen(req), sep=',')
-	return df
+    # Simulate a web browser request.
+    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    # Read the data into data frame
+    df = pd.read_csv(urllib.request.urlopen(req), sep=',')
+    return df
--- data/load_to_sqlite.py
+++ data/load_to_sqlite.py
@@ -1,18 +1,19 @@
 import sqlite3
 import os
 
+
 def load_to_database(df, table_name):
-	# Connect to the SQLite database or create a new one if it doesn't exist
-	db_file = 'database.sqlite3'
-	if not os.path.exists(db_file):
- 		# Create an empty database file
-		open(db_file, 'w').close()  
+    # Connect to the SQLite database or create a new one if it doesn't exist
+    db_file = 'data.sqlite'
+    if not os.path.exists(db_file):
+        # Create an empty database file
+        open(db_file, 'w').close()
 
   # Connect to the SQLite database
-	conn = sqlite3.connect(database=db_file)
+    conn = sqlite3.connect(database=db_file)
 
-	# Write the DataFrame to a SQLite table
-	df.to_sql(table_name, conn, if_exists='replace', index=False)
+    # Write the DataFrame to a SQLite table
+    df.to_sql(table_name, conn, if_exists='replace', index=False)
 
-	# Close the database connection
-	conn.close()
+    # Close the database connection
+    conn.close()
